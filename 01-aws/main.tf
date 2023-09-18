2d979e163252308161d28818c874b7f3339e6d91
Josiah <josiah_g@live.com>
2023-09-18 13:32:03 -0400

    Finished replacing infrastructure lab

--- 01-aws/main.tf
+++ 01-aws/main.tf
@@ -11,3 +11,24 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name" = "Main VPC"
+  }
+
+}
+
+
+# resource "aws_vpc" "myvpc" {
+#   cidr_block = "192.168.0.0/16"
+
+#   tags = {
+#     "Name" = "my vpc"
+#   }
+
+# }
---
+++ replacing_infrastructure/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+resource "aws_instance" "server" {
+    ami = "ami-04cb4ca688797756f"
+    instance_type = "t2.micro"
+    tags = {
+        "Name" = "my-server"
+    }
+}
+
+
+# To replace an already created instance we can use the below command:
+# terraform apply -replace="aws_instance.server" -auto-approve
+# terraform apply -replace="resource.localname" -auto-approve
+
