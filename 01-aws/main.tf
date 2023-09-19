9a8943dd1ebcbbd1ac117cc3250d6063f0fa14d2
Josiah <josiah_g@live.com>
2023-09-19 14:31:29 -0400

    Created route table and internet gateway

--- 01-aws/main.tf
+++ 01-aws/main.tf
@@ -14,9 +14,10 @@ provider "aws" {
 }
 
 
+# Create VPC
 
 resource "aws_vpc" "main" {
-  cidr_block =  var.vpc_cidr_block
+  cidr_block = var.vpc_cidr_block
 
   tags = {
     "Name" = "Production ${var.main_vpc_name}"
@@ -24,16 +25,40 @@ resource "aws_vpc" "main" {
 
 }
 
-
+# Create Subnet 
 resource "aws_subnet" "web" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.web_subnet_cidr_block
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.web_subnet_cidr_block
   availability_zone = "us-east-1a"
   tags = {
     "Name" = "Web subnet"
   }
 
 }
+
+# Create an Internet Gateway
+
+resource "aws_internet_gateway" "web_gateway" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    "Name" = "${var.main_vpc_name} Gateway"
+  }
+}
+
+# Create a Route Table
+
+resource "aws_default_route_table" "main_vpc_default_rt" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.web_gateway.id
+  }
+  tags = {
+    "Name" = "Main VPC Default Route Table"
+  }
+}
+
+
 
 # resource "aws_vpc" "myvpc" {
 #   cidr_block = "192.168.0.0/16"
